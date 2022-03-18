4b6f10f8156e1e136e34608c02a4b9a46f42658d
rudghska <rudghska@gmail.com>
2022-03-18 21:17:01 +0900

    마우스 드래그 슬라이드

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const SLIDE_SIZE = 600;
-
+const MOVE_XPOS = 100;
 const btn = document.querySelector('.buttons');
 const slider = document.querySelector('.slider');
 const slideElems = slider.children;
@@ -7,7 +7,10 @@ const nextBtn = document.querySelector('#next');
 const prevBtn = document.querySelector('#prev');
 
 let counter = 1;
-
+let currentPos = 600;
+let posX1 = 0;
+let posX2 = 0;
+let firstPosX = 0;
 let currentBtn = btn.children;
 let beforeBtn = currentBtn[0];
 
@@ -19,12 +22,12 @@ slider.style.transform = `translateX(-${SLIDE_SIZE * counter}px)`;
 function slideElement(counter) {
   let btnCounter = counter;
 
-  slider.style.transform = `translateX(${-counter * SLIDE_SIZE}px)`;
-
   if (btnCounter === 6) btnCounter = 1;
   if (btnCounter === 0) btnCounter = 5;
 
-  console.log(btnCounter);
+  currentPos = SLIDE_SIZE * counter;
+  slider.style.transform = `translateX(-${counter * SLIDE_SIZE}px)`;
+
   beforeBtn.style.backgroundColor = 'lightgrey';
   currentBtn[btnCounter - 1].style.backgroundColor = 'black';
   beforeBtn = currentBtn[btnCounter - 1];
@@ -32,19 +35,20 @@ function slideElement(counter) {
 
 btn.addEventListener('click', e => {
   if (e.target === e.currentTarget) return;
-  slider.style.transition = `transform 0.4s ease-in-out`;
+  slider.style.transition = `transform 0.3s ease-in-out`;
   counter = parseInt(e.target.classList[1]);
   slideElement(counter);
 });
 
 nextBtn.addEventListener('click', () => {
-  slider.style.transition = `transform 0.4s ease-in-out`;
-  ++counter === slideElems.length ? slideElement(counter--) : slideElement(counter);
+  slider.style.transition = `transform 0.3s ease-in-out`;
+  ++counter === slideElems.length ? slideElement(--counter) : slideElement(counter);
 });
 
 prevBtn.addEventListener('click', () => {
-  slider.style.transition = `transform 0.4s ease-in-out`;
-  --counter === slideElems.length ? slideElement(counter++) : slideElement(counter);
+  slider.style.transition = `transform 0.3s ease-in-out`;
+  if (counter === 0) counter = 1;
+  --counter === slideElems.length ? slideElement(++counter) : slideElement(counter);
 });
 
 slider.addEventListener('transitionend', e => {
@@ -56,4 +60,28 @@ slider.addEventListener('transitionend', e => {
     slider.style.transition = 'none';
     slideElement((counter = slideElems.length - 2));
   }
+});
+
+slider.addEventListener('mousedown', e => {
+  slider.style.transition = 'none';
+  firstPosX = posX1 = e.clientX;
+
+  document.onmousemove = e => {
+    posX2 = posX1 - e.clientX;
+    posX1 = e.clientX;
+    slider.style.transform = `translateX(-${(currentPos += posX2)}px)`;
+  };
+  document.onmouseup = e => {
+    slider.style.transition = `transform 0.3s ease-in-out`;
+
+    if (firstPosX - posX1 > MOVE_XPOS) {
+      slideElement(++counter);
+    } else if (firstPosX - posX1 < -MOVE_XPOS) {
+      slideElement(--counter);
+    } else {
+      slideElement(counter);
+    }
+    document.onmouseup = null;
+    document.onmousemove = null;
+  };
 });
